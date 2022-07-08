a6d07d8e6ad73a1e3847ffe70499e77de18f3a5a
Mark Hennessy <mhennessy116@gmail.com>
2022-07-08 15:54:27 +0200

    cleanup nav

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
-const links = [
-  { href: "https://github.com/vercel/next.js", label: "GitHub" },
-  { href: "https://nextjs.org/docs", label: "Docs" },
-];
-
 export default function Nav() {
   return (
     <nav>
-      <ul className="flex justify-between items-center p-8">
+      <ul className="flex justify-between items-center p-5">
         <li>
           <img className="inline" src="/images/menu.svg" />
         </li>
