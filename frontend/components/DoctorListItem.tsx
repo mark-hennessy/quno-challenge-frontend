52729b5ec3f7e19104bb0da53eb79061bc9c8260
Mark Hennessy <mhennessy116@gmail.com>
2022-07-09 16:14:55 +0200

    fix DoctorListItem grid layout

--- frontend/components/DoctorListItem.tsx
+++ frontend/components/DoctorListItem.tsx
@@ -7,75 +7,62 @@ type Props = {
   doctor: Doctor;
 };
 
-function DoctorListItemRow({ children }: PropsWithChildren<{}>) {
-  return (
-    <div
-      className="grid grid-flow-col gap-6"
-      style={{ gridTemplateColumns: "auto 1fr" }}
-    >
-      {children}
-    </div>
-  );
-}
-
 function DoctorHighlight({ children }: PropsWithChildren<{}>) {
   return <li className="flex items-start">{children}</li>;
 }
 
 export default function DoctorListItem({ doctor }: Props) {
   return (
-    <li className="grid gap-4">
-      <DoctorListItemRow>
-        <img className="w-24 h-24" src={doctor.avatarUrl} alt="avatar" />
-        <div>
-          <div className="text-xs leading-5 text-trueGray-700 font-bold uppercase mb-1">
-            Hair Transplant
-          </div>
-          <div className="text-2xl leading-7 text-cyan-900 mb-2">
-            {doctor.name}
-          </div>
-          <div className="flex items-center">
-            <Icon name="map-outline.svg" alt="address" size="xs" />
-            <span className="text-xs leading-5 text-trueGray-700">
-              {[doctor.city, doctor.country].join(", ")}
-            </span>
-          </div>
+    <li
+      className="grid gap-x-6 gap-y-4"
+      style={{ gridTemplateColumns: "auto 1fr" }}
+    >
+      <img className="w-24 h-24" src={doctor.avatarUrl} alt="avatar" />
+      <div>
+        <div className="text-xs leading-5 text-trueGray-700 font-bold uppercase mb-1">
+          Hair Transplant
         </div>
-      </DoctorListItemRow>
-      <DoctorListItemRow>
-        <div>
-          <div>{doctor.qunoScoreNumber}</div>
-          <div className="text-xs leading-5 text-black font-bold">
-            {doctor.qunoScoreText}
-          </div>
-          <div className="text-xs leading-4 text-trueGray-700 uppercase">
-            Qunoscore
-          </div>
+        <div className="text-2xl leading-7 text-cyan-900 mb-2">
+          {doctor.name}
         </div>
-        <ul>
-          <DoctorHighlight>
-            <Icon name="star.svg" alt="star icon" size="sm" />
-            <div>({doctor.reviews.length} reviews)</div>
-          </DoctorHighlight>
-          <DoctorHighlight>
-            <Icon name="check.svg" alt="checkmark" size="sm" />
-            <div>
-              {getAmountWithCounterWord(doctor.treatmentsLastYear, "treatment")}{" "}
-              last year
-            </div>
-          </DoctorHighlight>
-          <DoctorHighlight>
-            <Icon name="check.svg" alt="checkmark" size="sm" />
-            <div>
-              {getAmountWithCounterWord(doctor.yearsExperience, "year")} of
-              experience
-            </div>
-          </DoctorHighlight>
-        </ul>
-      </DoctorListItemRow>
-      <DoctorListItemRow>
-        <div>TODO</div>
-      </DoctorListItemRow>
+        <div className="flex items-center">
+          <Icon name="map-outline.svg" alt="address" size="xs" />
+          <span className="text-xs leading-5 text-trueGray-700">
+            {[doctor.city, doctor.country].join(", ")}
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <div>{doctor.qunoScoreNumber}</div>
+        <div className="text-xs leading-5 text-black font-bold">
+          {doctor.qunoScoreText}
+        </div>
+        <div className="text-xs leading-4 text-trueGray-700 uppercase">
+          Qunoscore
+        </div>
+      </div>
+      <ul>
+        <DoctorHighlight>
+          <Icon name="star.svg" alt="star icon" size="sm" />
+          <div>({doctor.reviews.length} reviews)</div>
+        </DoctorHighlight>
+        <DoctorHighlight>
+          <Icon name="check.svg" alt="checkmark" size="sm" />
+          <div>
+            {getAmountWithCounterWord(doctor.treatmentsLastYear, "treatment")}{" "}
+            last year
+          </div>
+        </DoctorHighlight>
+        <DoctorHighlight>
+          <Icon name="check.svg" alt="checkmark" size="sm" />
+          <div>
+            {getAmountWithCounterWord(doctor.yearsExperience, "year")} of
+            experience
+          </div>
+        </DoctorHighlight>
+      </ul>
+      <div>TODO</div>
+      <div>TODO</div>
     </li>
   );
 }
